a87ca2edb454f355c379b93b1e2c49bf1ece0ef9
evya61992 <evya61992@gmail.com>
2025-01-16 17:30:44 +0200

    added create user

--- controller/UsersController.js
+++ controller/UsersController.js
@@ -21,6 +21,15 @@ const getUser = async (req , res)=>{
         res.status(500).json({ message: "Error fetching users", error: error.message });
       }
 }
+const createUser = async(req,res)=>{
+    try {
+        const newUser = new userModel(req.body)
+        const savedUser = await newUser.save()
+        res.status(201).json({ message: "User created successfully", user: savedUser });
+      } catch (error) {
+        res.status(500).json({ message: "Error creating user", error: error.message });
+      }
+}
 
-module.exports = {getAllUsers, getUser}
+module.exports = {getAllUsers, getUser ,   createUser}
 
